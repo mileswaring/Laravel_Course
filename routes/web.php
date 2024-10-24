2488b63628902856efb9d922deaf0c5f85d326bf
Miles Waring <miles.g.waring@icloud.com>
2024-10-24 19:18:39 +0100

    Laravel Learning Day 7 Complete

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use \Illuminate\Support;
+
+use App\Models\Job;
+
+
+
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function (){
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
@@ -34,33 +22,8 @@ Route::get('/contact', function () {
 });
 
 
-Route::get('/jobs/{id}', function ($id) {
+Route::get('/jobs/{id}', function ($id){
 
-        $jobs = [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];
-
-        //short hand
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-    
-    // \Illuminate\Support\Arr::first($jobs, function ($job) use ($id){
-    //     return $job['id'] == $id;
-    // });
-
-
+    $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
