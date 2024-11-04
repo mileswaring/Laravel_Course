238c9b92ac319e7bb9541dc60214202cd4668180
Miles Waring <miles.g.waring@icloud.com>
2024-11-04 11:02:11 +0000

    Laravel Course Day 10 Completed

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,9 +1,11 @@
 <?php
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
 // : array is explicilty an array
 class Job extends Model{
+    use HasFactory;
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
